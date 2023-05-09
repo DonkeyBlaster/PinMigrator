05dd5df302c2ad1612240256836f72b417397db4
DonkeyBlaster <57310593+DonkeyBlaster@users.noreply.github.com>
2023-05-08 21:18:47 -0700

    finish up bot, add reaction support

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # pin-migrator
-Lightweight Discord bot that watches a channel for pins, then immediately unpins that message and sends a link to it in some other channel.
+Lightweight Discord bot that watches a server for pins, then immediately unpins that message and sends a link to it in some other channel.
+Also works with the ⭐ and 📌 reactions.
 
 ## Usage
 Clone the repo, then run `pip install -r requirements.txt` to install dependencies. 
-Rename `.env_example` to `.env` and place bot token there.
+Rename `.env_example` to `.env` and place bot token and pin channel ID there.
 Then run `python3 main.py` to start the bot.
 
 Built on Python 3.10, untested on other versions.
--- main.py
+++ main.py
@@ -5,10 +5,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
-watching_channel: int = int(os.getenv("WATCHING"))
 pins_channel: int = int(os.getenv("PINS"))
 
-client: discord.Client = discord.Client(intents=discord.Intents.all())
+intents = discord.Intents.default()
+intents.messages = True
+intents.typing = False
+intents.presences = False
+
+client: discord.Client = discord.Client(intents=intents)
 
 
 @client.event
@@ -17,40 +21,47 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print("------")
-    await client.change_presence(
-        activity=discord.Activity(type=discord.ActivityType.watching, name="for pins in #general"))
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="for pins"))
 
 
 @client.event
-async def on_message(message: discord.Message):
-    # why is this event required for the on_message_edit event to trigger???
-    pass
+async def on_raw_reaction_add(reaction: discord.RawReactionActionEvent):
 
-
-@client.event
-async def on_message_edit(before: discord.Message, after: discord.Message):
-    print("Message edited")
-    if (before.channel.id != watching_channel) or (after.channel.id != watching_channel):
+    if str(reaction.emoji) != "📌" and str(reaction.emoji) != "⭐":
         return
 
-    if not before.pinned and after.pinned:  # message was pinned
-        await after.unpin(reason="Pin migrated to pins channel")
-        await after.add_reaction("📌")
+    migration_channel: discord.TextChannel = client.get_channel(pins_channel)
+    if migration_channel is None:
+        print("Could not find pins channel")
+        return
 
-        migration_channel: discord.TextChannel = client.get_channel(pins_channel)
-        if migration_channel is None:
-            print("Could not find pins channel")
-            return
+    message: discord.Message = await client.get_channel(reaction.channel_id).fetch_message(reaction.message_id)
+    await migration_channel.send(embed=generate_embed(message))
+    await message.add_reaction("✅")
 
-        embed: discord.Embed = discord.Embed(title="Pinned Message", color=0x2f3136)
-        embed.add_field(name="Content", value=after.content, inline=False)
-        if len(after.attachments) > 0:
-            embed.add_field(name="Attachments",
-                            value="\n".join([attachment.url for attachment in after.attachments]), inline=False)
-        embed.set_author(name=after.author.display_name, icon_url=after.author.avatar)
-        embed.set_footer(
-            text=f"[Link]({after.jump_url})")
-        await migration_channel.send(embed=embed)
+
+@client.event
+async def on_raw_message_edit(payload: discord.RawMessageUpdateEvent):
+    migration_channel: discord.TextChannel = client.get_channel(pins_channel)
+    if migration_channel is None:
+        print("Could not find pins channel")
+        return
+
+    message: discord.Message = await client.get_channel(payload.channel_id).fetch_message(payload.message_id)
+    if message.pinned:
+        await migration_channel.send(embed=generate_embed(message))
+        await message.unpin()
+        await message.add_reaction("✅")
+
+
+def generate_embed(message: discord.Message) -> discord.Embed:
+    embed: discord.Embed = discord.Embed(color=0x2f3136)
+    embed.add_field(name=f"{message.jump_url}", value=message.content, inline=False)
+    if len(message.attachments) > 0:
+        embed.add_field(name="Attachments",
+                        value="\n".join([attachment.url for attachment in message.attachments]), inline=False)
+    embed.set_author(name=message.author.display_name, icon_url=message.author.avatar)
+    return embed
 
 
 client.run(TOKEN)
